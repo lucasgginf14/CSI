003060a3e117bbe391a2bd217c92100585dc8eda
Lucas <lucas.garcia.garcia@udc.es>
2026-02-04 18:41:53 +0100

    First commit

---
+++ code-review-p3-team-bana-corral-garcia-martinez-main/src/main/java/es/udc/fic/csi2324/baserest/BaseRestApplication.java
@@ -0,0 +1,13 @@
+package es.udc.fic.csi2324.baserest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseRestApplication.class, args);
+	}
+
+}
